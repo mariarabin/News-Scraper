57de0e60631efd4979fad98aa0c14bf7ec59feae
Maria Rabin <lrabin@gmail.com>
2019-07-24 23:26:33 -0400

    draft of server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// Dependencies
 var express = require('express');
 var exphbs = require('express-handlebars');
 var mongoose = require('mongoose');
@@ -7,10 +8,10 @@ var mongoose = require('mongoose');
 var PORT = process.env.PORT || 8083;
 var app = express();
 
-app.use(express.static(__dirname + '/public'));
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
-app.use(require('./controllers'));
+var db = require("./models");
+
+// Connect to the Mongo DB
+mongoose.connect("mongodb://localhost/populate", { useNewUrlParser: true });
 
 // mongoose
 mongoose.Promise = Promise;
@@ -31,6 +32,41 @@ db.once("open", function() {
     app.listen(PORT, function() {
         console.log("App running on port " + PORT);
     });
+});
+
+
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set Handlebars as the default templating engine.
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+//app.use('/static', express.static('public'));
+app.use(express.static(__dirname+'/public'));
+app.use(require('./controllers'));
+
+
+// Routes
+app.get("/", function (req, res) {
+    res.render("about");
+});
+app.get("/about", function (req, res) {
+    res.render("about");
+});
+
+app.get("/portfolio", function (req, res) {
+    res.render("portfolio")
+});
+
+app.get("/contact", function (req, res) {
+    res.render("contact");
+});
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function () {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
 });
 
 module.exports = app;
