ea8c764bf9bc05f177898ecbd2e4497e9c78da43
Maria Rabin <lrabin@gmail.com>
2019-07-28 08:20:21 -0400

    Updated the styling

--- public/assets/javascript/home.js
+++ public/assets/javascript/home.js
@@ -1,49 +1,36 @@
 $(function() {
-
     $('#scrapeArticlesButton').on("click", function(event) {
         event.preventDefault();
         $('.articlesScrapedBody').empty();
         $.ajax("/api/all", {
             type: "GET"
         }).then(function(response) {
-
             let oldLength = response;
-
-            console.log(oldLength);
-
             $.ajax("/api/scrape", {
                 type: "POST"
             }).then(function(response) {
 
-
                 $.ajax("/api/reduce", {
                     type: "DELETE"
                 }).then(function(response) {
-
                     let newText = $("<div>");
                     let newLength = response.length;
-
-                    console.log(newLength);
-
                     let numberChanged = parseInt(newLength) - parseInt(oldLength);
 
                     if (numberChanged == 0) {
-                        newText.text("Scraper is up to date")
+                        newText.text("News list is updated!")
                         $('.articlesScrapedBody').append(newText)
                         $('#scrapeArticlesModal').modal('show');
                     }
 
                     else {
-                        newText.text(numberChanged + " new articles scraped!")
+                        newText.text(numberChanged + " New Articles are added/scraped!")
                         $('.articlesScrapedBody').append(newText)
                         $('#scrapeArticlesModal').modal('show');
                     }
-
                 })
-
             })
         })
-
     });
 
     $("#closeScrapeButton").on("click", function(event) {
@@ -52,22 +39,19 @@ $(function() {
             type: "GET"
         }).then(function() {
             location.reload();
-            console.log("site updated")
         })
     });
 
     $('.saveArticleButton').on("click", function(event) {
         event.preventDefault();
-
         $('.articleSavedBody').empty();
-
         let articleId = $(this).data("id");
 
         $.ajax("/api/save/article/" + articleId, {
             type: "PUT"
         }).then(function() {
             let newText = $('<div>');
-            newText.text("Article now accessible through your Saved Articles");
+            newText.text("Pls. see 'Saved News' for the saved article.");
             $('.articleSavedBody').append(newText);
             $('#articleSavedModal').modal('show');
         })
@@ -79,7 +63,6 @@ $(function() {
         $.ajax("/", {
             type: "GET"
         }).then(function() {
-            console.log("site updated");
             location.reload();
         })
     });
